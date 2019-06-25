5ba0d6522f7de1b3cc3a0e15f9559690d6d56abb
Bernard Camus <berniecamus@gmail.com>
2019-06-25 11:40:52 +0100

    INFRA-6190 added requirements and jenkinsfile

---
+++ package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xeou
+yum install -y zip python37
+
+BASEDIR=/data
+PIPPACKAGESDIR=${BASEDIR}/lambda-packages
+
+cd ${BASEDIR}
+
+zip grant_ssh_access.zip grant_ssh_access.py
+
+mkdir -p ${PIPPACKAGESDIR}
+pip-3.7 install -t ${PIPPACKAGESDIR} -r requirements.txt
+cd ${PIPPACKAGESDIR}
+zip -r ../grant_ssh_access.zip .
