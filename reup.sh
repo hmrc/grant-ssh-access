213586d494083be6047b5874add77b41c86ef11a
Adam N <51328998+adamn-hmrc@users.noreply.github.com>
2019-08-16 10:49:36 +0100

    Use aws_lambda_logging

--- reup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-aws-profile -p deity \
-    aws lambda \
-    update-function-code \
-    --function-name arn:aws:lambda:eu-west-2:150648916438:function:grant-ssh-access \
-    --zip-file fileb://grant_ssh_access.zip
-
-
